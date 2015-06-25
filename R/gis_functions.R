d947bc26a9a9d52a33520a94f1d0b6ff99492cf6
Nick <goldingn35@gmail.com>
2015-06-25 14:06:16 +0100

    added insertRaster

--- R/gis_functions.R
+++ R/gis_functions.R
@@ -53,3 +53,59 @@ bufferMask <- function (mask, points, buffer = 0) {
   return (mask)
 
 }
+
+#' @name insertRaster
+#' @rdname insertRaster
+#'
+#' @title Insert new values into a Raster* template
+#'
+#' @description Given a template raster, new set of values for non-missing
+#'  cells and optional index for non-missing cells, create a Raster* object
+#'  containing the new values. The raster object will be replicate to have
+#'  as many layers as the new dataframe has columns.
+#'
+#' @param raster a Raster* object with non-NA cells in which to insert new_vals
+#' @param new_vals a dataframe of new values to put in the non-NA cells of
+#'  \code{raster}, with values from each row in a separate layer
+#' @param idx an optional index giving the cell numbers corresponding to the
+#'  rows of \code{new_vals}.
+#'
+#' @return a RasterBrick object with the same extent and resolution as
+#'  \code{raster} and as many layers as the number of columns in
+#'  \code{new_vals}.
+#'
+#' @family GIS
+#'
+insertRaster <- function (raster, new_vals, idx = NULL) {
+
+
+  # calculate cell index if not provided
+  if (is.null(idx)) idx <- getCellIdx(raster)
+
+  # check the index makes superficial sense
+  stopifnot(length(idx) == nrow(new_vals))
+  stopifnot(max(idx) <= ncell(raster))
+
+  # create results raster
+  n <- ncol(new_vals)
+  raster_new <- brick(replicate(n,
+                              raster[[1]],
+                              simplify = FALSE))
+  names(raster_new) <- colnames(new_vals)
+
+  # update the values
+  for(i in 1:n) {
+    raster_new[[i]][idx] <- new_vals[, i]
+  }
+
+  return (raster_new)
+
+}
+
+
+
+
+
+
+
+
